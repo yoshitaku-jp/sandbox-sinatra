ce56ddbca1fe5f2e9d469264382274e7244eb0f4
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-06-24 21:52:42 +0900

    一覧画面と詳細画面の遷移

---
+++ app.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+
+get '/' do
+    hash = {"id":1,"title": "メモ","text": "コレはメモの中身です"}
+    @id = hash[:id]
+    @title = hash[:title]
+    erb :index
+end
+
+get '/:id' do
+    hash = {"id":1,"title": "メモ","text": "コレはメモの中身です"}
+    @title = hash[:text]
+    erb :index
+end
